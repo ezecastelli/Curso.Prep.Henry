4a72483cf10aa3e452cebbfa3015b27573ee8dc9
ezecastelli <castelliezequiel@hotmail.com>
2021-02-08 10:49:51 -0300

    ACTUALIZACION HOMEWORK 04-JS-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -217,7 +217,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-}
+  var array = []
+  var suma = numero 
+  for (i = 0 ; i < 10; i++) {
+    suma = suma + 2;
+    if (suma === i) break; 
+    else {
+      array.push(suma);
+    }
+  }
+  if (i<10) {
+    return "Se interrumpió la ejecución";   
+  } else {
+    return array;
+  }
+  }
+
 
 
 function continueStatement(numero) {
@@ -227,6 +242,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for (i=0; i<10 ; i++) {
+    if (i === 5) continue;
+    else {
+    suma = suma + 2;
+    array.push(suma);
+  }
+}
+return array;
 }
 
 
